e8dfcb76d6ce06da9464de60ea4ea832d1540096
yas871 <yasminlenny855@gmail.com>
2024-10-02 16:50:50 -0300

    Add files via upload

---
+++ PROJETO_TESTE/js/main.js
@@ -0,0 +1,32 @@
+// Introdução ao JS
+// 1.1 - Variáveis
+var novavariavel;
+//let minhavariavel = "Olá Mundo";
+// 1.2 - Tipos de dados
+// 1.2.1 - Números
+let minhavariavel = 30;
+// 1.2.2 - string
+let meuNome = "Bonde do Tigrao";
+let alunoNovo = "Batista";
+let alunoturista = "Catatau N° 22"; 
+// 1.2.3 - Boolean
+let souLindo = true;
+let souCharmoso = true;
+let souChato = false;
+// 1.2.4 - Nulo
+let valorNulo = null;
+// 1.2.5 - Indefinido
+let valorIndefinido;
+// 1.2.6 - Objeto
+let minhaDataNascimento = new (1980,12,1);
+// 1.3 - Operadores
+let soma = 5 + 3;
+let diferenca = 26 - 2;
+let produto = 5 * 3;
+// 1.4 - Funções
+// 1.5 - Objetos
+// 1.6 - Arrays
+// 1.7 - Condicionais
+// 1.8 - Loops
+// 1.9 - Funções construtoras e objetos
+// 1.10 - Funções de array
